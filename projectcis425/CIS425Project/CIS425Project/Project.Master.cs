ec99166b9f9e5b752b57fba410c79c41f4d29d86
adobrons <adobrons@asu.edu>
2018-04-21 23:28:56 -0700

    edited the contact page

--- projectcis425/CIS425Project/CIS425Project/Project.Master.cs
+++ projectcis425/CIS425Project/CIS425Project/Project.Master.cs
@@ -16,6 +16,7 @@ namespace CIS425Project
 
         protected void SearchButton_Click(object sender, EventArgs e)
         {
+            string searchWord = SearchbarTextBox.Text;
 
         }
     }
